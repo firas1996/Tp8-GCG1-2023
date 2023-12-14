6458fe768f7ebf17bba5a4202ab68703f905fd8f
firas hamila <firas.hm.1996@gmail.com>
2023-12-14 10:14:20 +0100

    ss

---
+++ LibraryWebAppClient/Controllers/BookClientController.cs
@@ -0,0 +1,26 @@
+﻿using LibraryWebAppClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace LibraryWebAppClient.Controllers
+{
+    public class BookClientController : Controller
+    {
+        public async Task<IActionResult> GetAllBooks()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7237");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json")
+                );
+            HttpResponseMessage res =  await client.GetAsync("api/Books/get-all-books");
+            if(res.IsSuccessStatusCode)
+            {
+                var livres = await res.Content.ReadFromJsonAsync<IEnumerable<BookClient>>();
+                return View(livres);
+            }
+            return View();
+        }
+    }
+}
