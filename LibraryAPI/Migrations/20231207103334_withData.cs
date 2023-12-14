e705d915d5e836975b39bb318b65171fec39be83
firas hamila <firas.hm.1996@gmail.com>
2023-12-14 08:49:49 +0100

    Ajoutez des fichiers projet.

---
+++ LibraryAPI/Migrations/20231207103334_withData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LibraryAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class withData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "CoverUrl", "Description", "Rating", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Auther 1", "https://www.book1.com", "Description 1", 3.7000000000000002, "Book 1" },
+                    { 2, "Auther 2", "https://www.book2.com", "Description 2", 2.2000000000000002, "Book 2" },
+                    { 3, "Auther 3", "https://www.book3.com", "Description 3", 1.0, "Book 3" },
+                    { 4, "Auther 4", "https://www.book4.com", "Description 4", 5.0, "Book 4" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
---
+++ LibraryAPI/Models/LibraryDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryAPI.Models
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+                new Book { Id=1, Title="Book 1", Description="Description 1", Author="Auther 1", Rating=3.7, CoverUrl="https://www.book1.com"},
+                new Book { Id=2, Title="Book 2", Description="Description 2", Author="Auther 2", Rating=2.2, CoverUrl="https://www.book2.com"},
+                new Book { Id=3, Title="Book 3", Description="Description 3", Author="Auther 3", Rating=1, CoverUrl="https://www.book3.com"},
+                new Book { Id=4, Title="Book 4", Description="Description 4", Author="Auther 4", Rating=5, CoverUrl="https://www.book4.com"}
+                );
+        }
+    }
+}
